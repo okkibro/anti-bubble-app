581c77e88fdaa88a75de4a326e9f95ecaf79872f
Oscar Müller <okkibro@gmail.com>
2020-03-24 11:22:05 +0100

    Added "@types/jest" to package.json

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://127.0.0.1:27017/anti-bubble', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database is connected"))
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 
 module.exports = mongoose;
