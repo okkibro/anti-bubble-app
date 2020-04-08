7c11018bceae91e9079e949c1cab69021c30b129
5640059 <j.schut@students.uu.nl>
2020-04-08 15:11:30 +0200

    removed jasmine html reporter

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
 
