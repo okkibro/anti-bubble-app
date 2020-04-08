f10ca0bfd2776df74d408052d2ad40876395407c
5640059 <j.schut@students.uu.nl>
2020-04-08 14:58:47 +0200

    calling the right CLI function now

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
 
