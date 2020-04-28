98dc1c116af364fcc94e21ece02a9768d07aea0c
Inge <33689218+Inge573@users.noreply.github.com>
2020-04-28 10:55:18 +0200

    Simple shop loads

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -13,7 +13,7 @@ import { ShopService } from 'src/app/services/shop.service';
 
 export class ShopComponent implements OnInit {
 
-  shopDetails: Shop;
+  shopDetails: Shop[];
 
   constructor(private authenticationService: AuthenticationService, private shopService : ShopService) { }
 
@@ -24,7 +24,9 @@ export class ShopComponent implements OnInit {
   ngOnInit(): void {
     this.shopService.shop().subscribe(shop => {
       this.shopDetails = shop;
-      console.log(this.shopDetails);
+      console.log(this.shopDetails.length);
+      console.log(this.shopDetails[0].title);
+      this.loadTable();
   }, (err) => {
       console.error(err);
   });
@@ -32,11 +34,34 @@ export class ShopComponent implements OnInit {
   }
 
   loadTable(){
-    // var shopQueryRes : Shop[] = [] ;
-    // for(var i = 0; i < 8; i++){
-      
-    // }
-    this.shopDetails[0].image;
-    this.shopDetails[0].price;
+    for(var i = 0; i < this.shopDetails.length; i++){
+
+      console.log("gegroet");
+      var title = this.shopDetails[i].title;
+      var image = this.shopDetails[i].image;
+      var price = this.shopDetails[i].price;
+
+
+      var anchor = document.getElementById("shopAnchor");
+      var card1 = document.createElement("mat-card");
+      var cardContent1 = document.createElement("mat-card-content");
+      var image1 = document.createElement("img");
+      var cutLine = document.createElement("br");
+
+      image1.setAttribute("src", image);
+
+      var price1 = document.createElement("button");
+      var price1Text = document.createTextNode(price.toString());
+      price1.appendChild(price1Text);
+      var title1 = document.createTextNode(title);
+   
+      cardContent1.appendChild(image1);
+      cardContent1.appendChild(cutLine);
+      cardContent1.appendChild(title1);
+      cardContent1.appendChild(cutLine);
+      cardContent1.appendChild(price1);
+      card1.appendChild(cardContent1);
+      anchor.appendChild(card1);
+    }
   }
 }
