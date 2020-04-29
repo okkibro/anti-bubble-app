63a2e9efcaee8a3d557268d46bd362d2f158787f
Inge <33689218+Inge573@users.noreply.github.com>
2020-04-29 10:56:55 +0200

    New method for shop display

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -13,7 +13,7 @@ import { ShopService } from 'src/app/services/shop.service';
 
 export class ShopComponent implements OnInit {
 
-  shopDetails: Shop[] ;
+  shopDetails: Shop ;
 
   constructor(private authenticationService: AuthenticationService, private shopService : ShopService) { }
 
@@ -24,9 +24,9 @@ export class ShopComponent implements OnInit {
   ngOnInit(): void {
     this.shopService.shop().subscribe(shop => {
       this.shopDetails = shop;
-      console.log(this.shopDetails.length);
-      console.log(this.shopDetails[0].title);
-      this.loadTable();
+      // console.log(this.shopDetails.length);
+      // console.log(this.shopDetails[0].title);
+      // this.loadTable();
   }, (err) => {
       console.error(err);
   });
@@ -34,37 +34,57 @@ export class ShopComponent implements OnInit {
   }
   
  // Building the table for all elements in the shop
-  loadTable(){
-    for(var i = 0; i < this.shopDetails.length; i++){
+  // loadTable(){
+  //   for(var i = 0; i < this.shopDetails.length; i++){
 
-      //Puting the elements from the database in variables
-      var title = this.shopDetails[i].title;
-      var image = this.shopDetails[i].image;
-      var price = this.shopDetails[i].price;
+  //     //Puting the elements from the database in variables
+  //     var title = this.shopDetails[i].title;
+  //     var image = this.shopDetails[i].image;
+  //    var price = this.shopDetails[i].price;
 
-      //Making HTML elements
-      var anchor = document.getElementById("shopAnchor");
-      var card1 = document.createElement("mat-card");
-      var image1 = document.createElement("img");
-      var tableRow1 = document.createElement("tr");
-      var tableRow2 = document.createElement("tr");
-      var tableRow3 = document.createElement("tr");
+  //     //Making HTML elements
+  //     var anchor = document.getElementById("shopAnchor");
+  //     // var var1 = document.getElementById("row1");
+  //     // var var2 = document.getElementById("row2");
+  //     // var var3 = document.getElementById("row3");
+  //     var card1 = document.createElement("mat-card");
+  //     // card1.setAttribute("id",i.toString());
+  //     var image1 = document.createElement("img");
+  //     var tableRow1 = document.createElement("tr");
+  //     var tableRow2 = document.createElement("tr");
+  //     var tableRow3 = document.createElement("tr");
 
-      image1.setAttribute("src", image);
+  //     image1.setAttribute("src", image);
 
-      var price1 = document.createElement("button");
-      var price1Text = document.createTextNode(price.toString());
-      price1.appendChild(price1Text);
-      var title1 = document.createTextNode(title);
+  //     var price1 = document.createElement("button");
+  //     var price1Text = document.createTextNode(price.toString());
+  //     price1.appendChild(price1Text);
+  //     var title1 = document.createTextNode(title);
    
-      //Building actual table 
-      tableRow1.appendChild(image1);
-      tableRow2.appendChild(title1);
-      tableRow3.appendChild(price1);
-      card1.appendChild(tableRow1);
-      card1.appendChild(tableRow2);
-      card1.appendChild(tableRow3);
-      anchor.appendChild(card1);
-    }
-  }
+  //     //Building actual table 
+  //     tableRow1.appendChild(image1);
+  //     tableRow2.appendChild(title1);
+  //     tableRow3.appendChild(price1);
+  //     // var1.appendChild(tableRow1);
+  //     // var2.appendChild(tableRow2);
+  //     // var3.appendChild(tableRow3);
+  //     card1.appendChild(tableRow1);
+  //     card1.appendChild(tableRow2);
+  //     card1.appendChild(tableRow3);
+  //     anchor.appendChild(card1);
+  //   }
+
+    // for(var i = 0; i < this.shopDetails.length; i++){
+
+    //   //Puting the elements from the database in variables
+    //   var currentCard = document.getElementById(i.toString());
+    //   var button = document.getElementById("buttonId");
+
+    //   var price = this.shopDetails[i].price;
+
+    //   var price1Text = document.createTextNode(price.toString());
+    //   button.appendChild(price1Text);
+    //   currentCard.appendChild(button);
+    // }
+  // } 
 }
