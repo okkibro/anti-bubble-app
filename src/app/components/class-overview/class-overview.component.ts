049f2a7472b58ac6727284dacaa3fcb38102cf49
Steven Jaspersen <s.m.jaspersen@students.uu.nl>
2020-05-05 16:31:28 +0200

    search function implemented

--- src/app/components/class-overview/class-overview.component.ts
+++ src/app/components/class-overview/class-overview.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { User } from '../../models/user';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'mean-class-overview',
@@ -11,9 +12,13 @@ import { User } from '../../models/user';
 })
 export class ClassOverviewComponent implements OnInit {
 
+  searchBar = this.fb.group({
+    query: ['', []]
+  });
+
   classmates: User[];
 
-  constructor(private auth: AuthenticationService, private router: Router) { }
+  constructor(private auth: AuthenticationService, private router: Router, private fb: FormBuilder) { }
 
   logoutButton() {
     return this.auth.logout();
@@ -23,5 +28,17 @@ export class ClassOverviewComponent implements OnInit {
     this.auth.getAllClassmates().subscribe((data) => {
       this.classmates = data;
     });
+  }
+
+  search() {
+    let query:string = this.searchBar.get('query').value;
+    let table = document.getElementById("table").childNodes;
+    for (let i:number = 0; i < this.classmates.length; i++) {
+      if (this.classmates[i].firstName.includes(query) || this.classmates[i].lastName.includes(query)) {
+        (table[i + 1] as HTMLElement).style.display = "";
+      } else {
+        (table[i + 1] as HTMLElement).style.display = "none";
+      }
+    }
   }
 }
