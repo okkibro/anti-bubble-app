77d55d842c3344a5d5082c0269d944042d616923
Mahler <beaumahler@hotmail.nl>
2020-05-08 16:25:30 +0200

    updates on trophy page

--- src/app/components/trophies/trophies.component.ts
+++ src/app/components/trophies/trophies.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'mean-trophies',
@@ -10,7 +11,37 @@ export class TrophiesComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    var trophy_Image: string = "../../../assets/images/trophy_vb.jpg";
+    var image = document.createElement("img");
+    image.setAttribute("src", trophy_Image);
+    image.style.height = "25vh";                    
+    image.style.width = "15vw"; 
+    
+    var tabje = document.getElementById("matTab");
+    tabje.appendChild(image);
+  }
+
+  
+  progressBar(){
+
+    var achievedTable = document.getElementById("achievedTable").getElementsByTagName('tr');
+    var notAchievedTable = document.getElementById("notYetAchievedTable").getElementsByTagName('tr');
+  
+    var achievedRowCount = achievedTable.length;
+    var notAchievedRowCount = notAchievedTable.length;
+
+    var totalAchievements = achievedRowCount + notAchievedRowCount;
+    var progress = achievedRowCount / totalAchievements * 100;
+   
+   
+    console.log(progress);
+    return progress;
   }
 
 }
+
+
+
+
