da9062e4ddb2f1a927dca933d52f01e735975800
Steven Jaspersen <s.m.jaspersen@students.uu.nl>
2020-05-11 15:23:24 +0200

    Avatar Page shows list of items owned in their corresponding category tab

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from '../../services/authentication.service';
 import { User } from '../../models/user';
 import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { MatTab } from '@angular/material/tabs';
+import { ShopService } from 'src/app/services/shop.service';
 
 @Component({
     selector: 'mean-avatar',
@@ -13,21 +14,32 @@ import { MatTab } from '@angular/material/tabs';
 export class AvatarComponent implements OnInit {
 
     userDetails: User;
+    itemsShown = [];
 
-    constructor(private auth: AuthenticationService) { }
+    constructor(private auth: AuthenticationService, private shopService: ShopService) { }
 
     logoutButton() {
         return this.auth.logout();
     }
 
     ngOnInit() {
-        //display skin colors on init
-        var collection = document.getElementById("itemCollection");
-        var sc_Images: string[] = ["../../../assets/images/avatarpage/brown.jpg",
-        "../../../assets/images/avatarpage/beige.png",
-        "../../../assets/images/avatarpage/darkbrown.jpg"];
+        // //display skin colors on init
+        // var collection = document.getElementById("itemCollection");
+        // var sc_Images: string[] = ["../../../assets/images/avatarpage/brown.jpg",
+        // "../../../assets/images/avatarpage/beige.png",
+        // "../../../assets/images/avatarpage/darkbrown.jpg"];
 
-        collection.appendChild(this.applyTabData(sc_Images)); 
+        // collection.appendChild(this.applyTabData(sc_Images)); 
+
+        this.shopService.shop("alles").subscribe(shop => {
+            this.auth.profile().subscribe(user => {
+                for (let i = 0; i < shop.length; i++) {
+                    if (user.inventory.find(x => x._id == shop[i]._id) != null) {
+                        this.itemsShown.push(shop[i]);
+                    }
+                }
+            })
+        })
     }
 
     tabChange(event) {
@@ -35,30 +47,30 @@ export class AvatarComponent implements OnInit {
         var collection = document.getElementById("itemCollection");
 
         //replace existing images with the images corresponding to the clicked tab
-        if (currentTab == "Huidskleur") {
-            var sc_Images: string[] = ["../../../assets/images/avatarpage/brown.jpg",
-            "../../../assets/images/avatarpage/beige.png",
-            "../../../assets/images/avatarpage/darkbrown.jpg"];
+        // if (currentTab == "Huidskleur") {
+        //     var sc_Images: string[] = ["../../../assets/images/avatarpage/brown.jpg",
+        //     "../../../assets/images/avatarpage/beige.png",
+        //     "../../../assets/images/avatarpage/darkbrown.jpg"];
 
-            collection.lastChild.replaceWith(this.applyTabData(sc_Images));
-        }
-        else if (currentTab == "Kleding") {
-            var cl_Images: string[] = ["../../../assets/images/avatarpage/pants.png",
-            "../../../assets/images/avatarpage/shirt.png",
-            "../../../assets/images/avatarpage/dress.png"];
+        //     collection.lastChild.replaceWith(this.applyTabData(sc_Images));
+        // }
+        // else if (currentTab == "Kleding") {
+        //     var cl_Images: string[] = ["../../../assets/images/avatarpage/pants.png",
+        //     "../../../assets/images/avatarpage/shirt.png",
+        //     "../../../assets/images/avatarpage/dress.png"];
 
-            collection.lastChild.replaceWith(this.applyTabData(cl_Images));
-        }
-        else {
-            var hat_Images: string[] = ["../../../assets/images/avatarpage/redHat1.png",
-            "../../../assets/images/avatarpage/blueHat2.png",
-            "../../../assets/images/avatarpage/blueHat4.png",
-            "../../../assets/images/avatarpage/blueHat5.png",
-            "../../../assets/images/avatarpage/greenHat2.png",
-            "../../../assets/images/avatarpage/greenHat3.png"];
+        //     collection.lastChild.replaceWith(this.applyTabData(cl_Images));
+        // }
+        // else {
+        //     var hat_Images: string[] = ["../../../assets/images/avatarpage/redHat1.png",
+        //     "../../../assets/images/avatarpage/blueHat2.png",
+        //     "../../../assets/images/avatarpage/blueHat4.png",
+        //     "../../../assets/images/avatarpage/blueHat5.png",
+        //     "../../../assets/images/avatarpage/greenHat2.png",
+        //     "../../../assets/images/avatarpage/greenHat3.png"];
 
-            collection.lastChild.replaceWith(this.applyTabData(hat_Images));
-        }
+        //     collection.lastChild.replaceWith(this.applyTabData(hat_Images));
+        //}
     }
 
     applyTabData(images: string[]) {
