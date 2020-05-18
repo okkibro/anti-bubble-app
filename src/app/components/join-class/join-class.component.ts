13341f2832ef10cf2d20fe2fa1053b1bde294c6e
Steven Jaspersen <s.m.jaspersen@students.uu.nl>
2020-05-18 15:51:45 +0200

    added snackbar popups when submitting a classcode + added snackbar style to general styles

--- src/app/components/join-class/join-class.component.ts
+++ src/app/components/join-class/join-class.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { ClassesService } from '../../services/classes.service'
+import { ClassesService } from '../../services/classes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'mean-join-class',
   templateUrl: './join-class.component.html',
-  styleUrls: ['./join-class.component.css']
-})
+  styleUrls: ['./join-class.component.css',
+              '../../shared/general-styles.css']})
+
 export class JoinClassComponent implements OnInit {
   classCodeField = this.fb.group({
     classCode: ['', []]
   });
   value;
-  constructor(private fb: FormBuilder, private classesService: ClassesService) { }
+  constructor(private fb: FormBuilder, private classesService: ClassesService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   joinClass(): void {
     this.classesService.joinClass(this.value).subscribe(data => {
-      console.log(data);
+      if (data.succes) {
+        this.snackBar.open(data.message, 'X', {duration: 2500, panelClass: ['style-succes'], });
+      } else {
+        this.snackBar.open(data.message, 'X', {duration: 2500, panelClass: ['style-error'], });
+      }
     });
   }
-
 }
