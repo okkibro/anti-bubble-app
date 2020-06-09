ea62e37c3416316000fa773bdeebbef47a494ae7
Tijmen van der Spijk <tijmenspijk@gmail.com>
2020-06-09 16:01:03 +0200

    forgot some files

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// Method to activate and connect the local database to a url so express can use it.
+/** Method to activate and connect the local database to a url so express can use it. */ 
 mongoose.connect('mongodb://127.0.0.1:27017/anti-bubble', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database is connected"))
     .catch((error) => console.log(error));
