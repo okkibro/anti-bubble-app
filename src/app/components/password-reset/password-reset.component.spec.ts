df3f8513d2ae3d99c754f29b00174be2f3471a7e
Schut,J. (Jeroen) <j.schut@students.uu.nl>
2020-06-11 18:07:52 +0200

    Update password-reset.component.spec.ts with Unit Tests

--- src/app/components/password-reset/password-reset.component.spec.ts
+++ src/app/components/password-reset/password-reset.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PasswordRecoveryService } from '../../services/password-recovery.service';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
+import { ReactiveFormsModule, FormBuilder, FormsModule  } from '@angular/forms';
 
 import { PasswordResetComponent } from './password-reset.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('PasswordResetComponent', () => {
   let component: PasswordResetComponent;
   let fixture: ComponentFixture<PasswordResetComponent>;
+  const passwordRecoveryServiceStub: jasmine.SpyObj<PasswordRecoveryService> = jasmine.createSpyObj(
+    'passwService',
+    ['postNewPassword']
+  );
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PasswordResetComponent ]
+      declarations: [ PasswordResetComponent, ActivatedRoute ],
+      providers: [
+        {
+          provide: PasswordRecoveryService,
+          useValue: passwordRecoveryServiceStub
+        }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -17,5 +33,9 @@ describe('PasswordResetComponent', () => {
     fixture = TestBed.createComponent(PasswordResetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('Component successfully created', () => {
+    expect(component).toBeTruthy();
   });
 });
