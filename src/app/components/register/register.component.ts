f718fdc62d8a13ec904d4a8e68c3b12b6920087e
Oscar Müller <okkibro@gmail.com>
2020-06-17 13:27:28 +0200

    Some merge fixes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,12 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
 import { User } from '../../models/user';
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthenticationService} from '../../services/authentication.service';
-import {Router} from '@angular/router';
-import {User} from '../../models/user';
-import { Role } from 'src/app/models/role';
 import { ShopService } from 'src/app/services/shop.service';
 
 @Component({
