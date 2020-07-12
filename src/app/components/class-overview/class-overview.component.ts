60068c7a12826018142ac9f73693c59338b1103e
Oscar Müller <okkibro@gmail.com>
2020-07-12 15:47:27 +0200

    Change class-overview profile link of yourself to own profile page

--- src/app/components/class-overview/class-overview.component.ts
+++ src/app/components/class-overview/class-overview.component.ts
@@ -12,57 +12,61 @@ import { FormBuilder } from '@angular/forms';
 import { ClassesService } from '../../services/classes.service';
 
 @Component({
-  selector: 'mean-class-overview',
-  templateUrl: './class-overview.component.html',
-  styleUrls: ['./class-overview.component.css',
-    '../../shared/general-styles.css']
+    selector: 'mean-class-overview',
+    templateUrl: './class-overview.component.html',
+    styleUrls: ['./class-overview.component.css',
+        '../../shared/general-styles.css']
 })
 
 export class ClassOverviewComponent implements OnInit {
-  searchBar = this.fb.group({
-    query: ['', []]
-  });
-
-  classmates: User[];
-  userClassTitle;
-
-  public value: string;
-
-  constructor(
-    private classService: ClassesService,
-    private auth: AuthenticationService,
-    private router: Router,
-    private fb: FormBuilder
-  ) { }
-
-  ngOnInit() {
-    this.classService.getClass().subscribe((data) => {
-      if (data.succes) {
-        this.userClassTitle = data.class.title;
-        this.classmates = data.classmates;
-      }
+    searchBar = this.fb.group({
+        query: ['', []]
     });
-  }
 
-  /** Method to filter the students in a class. */
-  search() {
-    let query: string = this.searchBar.get('query').value.toLowerCase();
-    let table = document.getElementById('table').childNodes;
-    for (let i: number = 0; i < this.classmates.length; i++) {
-      if (this.classmates[i].firstName.toLowerCase().includes(query) || this.classmates[i].lastName.toLowerCase().includes(query)) {
-        (table[i + 1] as HTMLElement).style.display = '';
-      } else {
-        (table[i + 1] as HTMLElement).style.display = 'none';
-      }
-    }
-  }
+    classmates: User[];
+    userClassTitle;
+    userDetails: User;
 
-  /** Method to clear the filter so all students are displayed again. */
-  clear() {
-    this.value = '';
-    let table = document.getElementById('table').childNodes;
-    for (let i: number = 0; i < this.classmates.length; i++) {
-      (table[i + 1] as HTMLElement).style.display = '';
+    public value: string;
+
+    constructor(
+        private classService: ClassesService,
+        private auth: AuthenticationService,
+        private router: Router,
+        private fb: FormBuilder
+    ) { }
+
+    ngOnInit() {
+        this.classService.getClass().subscribe((data) => {
+            if (data.succes) {
+                this.userClassTitle = data.class.title;
+                this.classmates = data.classmates;
+            }
+        });
+        this.auth.profile().subscribe(user => {
+            this.userDetails = user;
+        })
     }
-  }
+
+    /** Method to filter the students in a class. */
+    search() {
+        let query: string = this.searchBar.get('query').value.toLowerCase();
+        let table = document.getElementById('table').childNodes;
+        for (let i: number = 0; i < this.classmates.length; i++) {
+            if (this.classmates[i].firstName.toLowerCase().includes(query) || this.classmates[i].lastName.toLowerCase().includes(query)) {
+                (table[i + 1] as HTMLElement).style.display = '';
+            } else {
+                (table[i + 1] as HTMLElement).style.display = 'none';
+            }
+        }
+    }
+
+    /** Method to clear the filter so all students are displayed again. */
+    clear() {
+        this.value = '';
+        let table = document.getElementById('table').childNodes;
+        for (let i: number = 0; i < this.classmates.length; i++) {
+            (table[i + 1] as HTMLElement).style.display = '';
+        }
+    }
 }
